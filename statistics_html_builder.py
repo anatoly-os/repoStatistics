60e4fd2b363cb4d52192cfa5c8e7421a094331e2
anatoly-os <osokingauss@gmail.com>
2016-04-10 15:59:48 +0600

    added table formatting + total added/removed lines computation

--- statistics_html_builder.py
+++ statistics_html_builder.py
@@ -14,14 +14,22 @@ def buildHtmlStatistics(authorStatDict):
     outputResults.write('  <body>' + '\n')
     outputResults.write('    <center>' + '\n')
     outputResults.write('      <table Align="center" Border="2" width="300">' + '\n')
+    outputResults.write('        <tr>' + '\n')
+    outputResults.write('          <td><b>Revision</b></td><td><b>Added</b></td><td><b>Removed</b></td>' + '\n')
+    outputResults.write('        </tr>' + '\n')
+    totalAdded = 0
+    totalRemoved = 0
     for item in authorStatDict[author]:
+      totalAdded += item[1]
+      totalRemoved += item[2]
       outputResults.write('        <tr>' + '\n')
-      outputResults.write('          <td>')
-      outputResults.write(str(item))
-      outputResults.write('          </td>' + '\n')
+      outputResults.write('          <td>{0}</td><td>{1}</td><td>{2}</td>'.format(item[0], item[1], item[2]) + '\n')
       outputResults.write('        </tr>' + '\n')
       outputResults.write('\n')
 
+    outputResults.write('        <tr>' + '\n')
+    outputResults.write('          <td>Total:</td><td>{0}</td><td>{1}</td>'.format(totalAdded, totalRemoved) + '\n')
+    outputResults.write('        </tr>' + '\n')
     outputResults.write('      </table>' + '\n')
     outputResults.write('    </center>' + '\n')
     outputResults.write('  </body>' + '\n')
