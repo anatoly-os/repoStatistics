2a7e33bb0d4c37bf74023b8141a5d024d182b9a2
anatoly-os <osokingauss@gmail.com>
2016-04-10 22:04:59 +0600

    added all developers page, ref to it in each developer statistics page. Added total added xml lines and number of remaining files in statistics as separate columns

--- libs/diffs_analyzer.py
+++ libs/diffs_analyzer.py
@@ -3,12 +3,39 @@ import os
 import os.path
 import re
 
+def linesCounter(linesByFiles):
+  charsToRemoveToIdentifyNonEmptyString = ['{', '}', '/', '*', '(', ')', '[', ']', ';','\n','\r', '+', '-']
+  rx = '[' + re.escape(''.join(charsToRemoveToIdentifyNonEmptyString)) + ']'
+  addedLines = 0
+  removedLines = 0
+  for codeFileLines in linesByFiles:
+    for line in codeFileLines:
+      if len(line) < 1:
+        continue
+
+      firstChar = line[0]
+      #svn diff uses '+' as a first char for added lines and '-' for removed ones
+      if not firstChar == '+' and not firstChar == '-':
+        continue
+
+      #service lines like "+++ path/to/src/file.cpp (revision xxx)" in diff files
+      #service lines like "--- path/to/src/file.cpp (revision xxx)" in diff files
+      if (len(line) > 2) and line[1] == line[2] == firstChar:
+        continue
+
+      remainingLine = re.sub(rx, '', line)
+      if (len(remainingLine) > 0):
+        if firstChar == '+':
+          addedLines += 1
+        elif firstChar == '-':
+          removedLines += 1
+
+  return addedLines, removedLines
+
 def analyze_diffs(revisionAuthorDict, authorDataDict):
   repoPath = os.getenv('RSSTAT_REPO_PATH')
   diffsPath = './diffs/'
   folderName = os.path.basename(repoPath)
-  charsToRemoveToIdentifyNonEmptyString = ['{', '}', '/', '*', '(', ')', '[', ']', ';','\n','\r', '+', '-']
-  rx = '[' + re.escape(''.join(charsToRemoveToIdentifyNonEmptyString)) + ']'
   revisionsNumSorted = sorted(revisionAuthorDict)
   for revision in revisionsNumSorted:
     file = open(diffsPath + '{0}_{1}.log'.format(folderName, str(revision)))
@@ -29,6 +56,8 @@ def analyze_diffs(revisionAuthorDict, authorDataDict):
 
     #print(revisionDiffByFiles)
     linesFromCodeFiles = []
+    linesFromXmlFiles = []
+    numberOfRemainingFiles = 0
     for fileNum in revisionDiffByFiles:
       if (len(revisionDiffByFiles[fileNum]) < 3):
         continue
@@ -39,38 +68,18 @@ def analyze_diffs(revisionAuthorDict, authorDataDict):
 
       fileName = os.path.basename(fileDefinition.split(' ')[1].split('\t')[0])
       codeFile = fileName.endswith('.cpp') or fileName.endswith('.h') or fileName.endswith('.txt') or fileName.endswith('.lsp')
-      if not codeFile:
-        #del revisionDiffByFiles[fileNum]
-        continue
+      if codeFile:
+        linesFromCodeFiles.append(revisionDiffByFiles[fileNum])
+      elif fileName.endswith('.xml'):
+        linesFromXmlFiles.append(revisionDiffByFiles[fileNum])
+      else:
+        numberOfRemainingFiles += 1
 
-      linesFromCodeFiles.append(revisionDiffByFiles[fileNum])
-
-    addedLines = 0
-    removedLines = 0
-    for codeFileLines in linesFromCodeFiles:
-      for line in codeFileLines:
-        if len(line) < 1:
-          continue
-
-        firstChar = line[0]
-        #svn diff uses '+' as a first char for added lines and '-' for removed ones
-        if not firstChar == '+' and not firstChar == '-':
-          continue
-
-        #service lines like "+++ path/to/src/file.cpp (revision xxx)" in diff files
-        #service lines like "--- path/to/src/file.cpp (revision xxx)" in diff files
-        if (len(line) > 2) and line[1] == line[2] == firstChar:
-          continue
-
-        remainingLine = re.sub(rx, '', line)
-        if (len(remainingLine) > 0):
-          if firstChar == '+':
-            addedLines += 1
-          elif firstChar == '-':
-            removedLines += 1
+    addedLines, removedLines = linesCounter(linesFromCodeFiles)
+    xmlLinesAdded, xmlLinesRemoved = linesCounter(linesFromXmlFiles)
 
     authorName = revisionAuthorDict[revision]
     if not authorName in authorDataDict:
       authorDataDict[authorName] = []
 
-    authorDataDict[authorName].append([revision, addedLines, removedLines])
+    authorDataDict[authorName].append([revision, addedLines, removedLines, xmlLinesAdded, numberOfRemainingFiles])
--- libs/statistics_html_builder.py
+++ libs/statistics_html_builder.py
@@ -4,7 +4,26 @@ import os.path
 def buildHtmlStatistics(authorStatDict):
   if not os.path.exists('./statistics'):
     os.makedirs('./statistics')
+
+  indexFile = open('./statistics/index.html', 'w+')
+  indexFile.write('<!doctype html>' + '\n')
+  indexFile.write('<html>' + '\n')
+  indexFile.write('  <head>' + '\n')
+  indexFile.write('    <title>Developers statistics</title>' + '\n')
+  indexFile.write('  </head>' + '\n')
+  indexFile.write('  <body>' + '\n')
+  indexFile.write('    <center>' + '\n')
+  indexFile.write('      <table Align="center" Border="2" width="300">' + '\n')
+  indexFile.write('        <tr align="center">' + '\n')
+  indexFile.write('          <td><b>Developer Nickname</b></td>' + '\n')
+  indexFile.write('        </tr>' + '\n')
   for author in authorStatDict:
+    #add developer to the index page
+    indexFile.write('        <tr align="center">' + '\n')
+    indexFile.write('          <td><a href="{0}.html"><i>{0}</i></a></td>'.format(author) + '\n')
+    indexFile.write('        </tr>' + '\n')
+
+    #add personal developer statistics
     outputResults = open('./statistics/{0}.html'.format(author), 'w+')
     outputResults.write('<!doctype html>' + '\n')
     outputResults.write('<html>' + '\n')
@@ -13,25 +32,35 @@ def buildHtmlStatistics(authorStatDict):
     outputResults.write('  </head>' + '\n')
     outputResults.write('  <body>' + '\n')
     outputResults.write('    <center>' + '\n')
-    outputResults.write('      <table Align="center" Border="2" width="300">' + '\n')
-    outputResults.write('        <tr>' + '\n')
-    outputResults.write('          <td><b>Revision</b></td><td><b>Added</b></td><td><b>Removed</b></td>' + '\n')
+    outputResults.write('      <table Align="center" Border="2" width="600">' + '\n')
+    outputResults.write('        <tr align="center">' + '\n')
+    outputResults.write('          <td><b>Revision</b></td><td><b>Added</b></td><td><b>Removed</b></td><td><b>Xml Lines Added</b></td><td><b>.sat, .osm, .dwg, etc.</b></td>' + '\n')
     outputResults.write('        </tr>' + '\n')
     totalAdded = 0
     totalRemoved = 0
+    totalXmlAdded = 0
+    totalOtherFilesNum = 0
     for item in authorStatDict[author]:
       totalAdded += item[1]
       totalRemoved += item[2]
-      outputResults.write('        <tr>' + '\n')
-      outputResults.write('          <td>{0}</td><td>{1}</td><td>{2}</td>'.format(item[0], item[1], item[2]) + '\n')
+      totalXmlAdded += item[3]
+      totalOtherFilesNum += item[4]
+      outputResults.write('        <tr align="center">' + '\n')
+      outputResults.write('          <td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td>'.format(item[0], item[1], item[2], item[3], item[4]) + '\n')
       outputResults.write('        </tr>' + '\n')
-      outputResults.write('\n')
 
-    outputResults.write('        <tr>' + '\n')
-    outputResults.write('          <td>Total:</td><td>{0}</td><td>{1}</td>'.format(totalAdded, totalRemoved) + '\n')
+    outputResults.write('        <tr align="center">' + '\n')
+    outputResults.write('          <td>Total:</td><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td>'.format(totalAdded, totalRemoved, totalXmlAdded, totalOtherFilesNum) + '\n')
+    outputResults.write('        </tr>' + '\n')
+    outputResults.write('        <tr align="center">' + '\n')
+    outputResults.write('          <td><a href="index.html"><i>Developers List</i></td>' + '\n')
     outputResults.write('        </tr>' + '\n')
     outputResults.write('      </table>' + '\n')
     outputResults.write('    </center>' + '\n')
     outputResults.write('  </body>' + '\n')
     outputResults.write('</html>' + '\n')
-    outputResults.write('\n')
+
+  indexFile.write('      </table>' + '\n')
+  indexFile.write('    </center>' + '\n')
+  indexFile.write('  </body>' + '\n')
+  indexFile.write('</html>' + '\n')
